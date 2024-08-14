470e2e23482495f04582b7bb5d8f3d56b1ab6778
Jonathan Ybema <40180069+Jonayb@users.noreply.github.com>
2024-08-14 19:41:07 +0200

    Update main.py
    - Rename non_edited to original
    - Fix small issues

--- files/main.py
+++ files/main.py
@@ -4,18 +4,19 @@ from PIL import Image
 from helpers import create_folders, search_media, set_exif
 
 
-def main_process(browser_path, window, edited_word="-editado"):
+def main_process(browser_path, window, edited_word):
     piexif_codecs = {'tif', 'tiff', 'jpeg', 'jpg'}
     media_moved = []  # List of already matched media
     matched_media_path = os.path.join(browser_path, "Matched")
-    non_edited_media_path = os.path.join(browser_path, "Originals")
+    original_media_path = os.path.join(browser_path, "Originals")
+    edited_word = edited_word or '-editado'
     error_counter = 0
     success_counter = 0
 
     try:
         # Convert iterator to a list and sort by length to process shorter names first
         files = sorted(os.scandir(browser_path), key=lambda s: len(s.name))
-        create_folders(matched_media_path, non_edited_media_path)
+        create_folders(matched_media_path, original_media_path)
     except Exception as e:
         window['-PROGRESS_LABEL-'].update("Choose a valid directory", visible=True, text_color='red')
         return
@@ -40,13 +41,13 @@ def main_process(browser_path, window, edited_word="-editado"):
             title_original = data.get('title', '')
 
             try:
-                title = search_media(browser_path, title_original, media_moved, non_edited_media_path, edited_word)
+                title = search_media(browser_path, title_original, media_moved, original_media_path, edited_word)
             except Exception as e:
                 print(f"Error in search_media() with file {title_original}: {e}")
                 error_counter += 1
                 continue
 
-            if title == "None":
+            if title is None or title == 'None':
                 print(f"{title_original} not found")
                 error_counter += 1
                 continue
@@ -91,5 +92,5 @@ def main_process(browser_path, window, edited_word="-editado"):
     window['-PROGRESS_BAR-'].update(100, visible=True)
     window['-PROGRESS_LABEL-'].update(
         f"Matching process finished with {success_counter}{success_message} and {error_counter}{error_message}.",
-        visible=True, text_color='#c0ffb3'
+        visible=True
     )
