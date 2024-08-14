70c5eeeac57ec2aa3c0e0543c6288cee05a38a04
Jonathan Ybema <40180069+Jonayb@users.noreply.github.com>
2024-08-14 19:41:51 +0200

    Update window.py
    - Add local testing functionality
    - Put code in methods for readability

--- files/window.py
+++ files/window.py
@@ -1,41 +1,50 @@
 import PySimpleGUI as sg
-from main import main_process  # Assuming you renamed mainProcess to main_process
+from main import main_process
 
-# Set the theme for the window
-sg.theme("SandyBeach")
 
-# Define the layout for the window
-layout = [
-    [sg.Text('Enter suffix used for edited photos (optional):')],
-    [sg.InputText(key='-INPUT_TEXT-'), sg.Button('Help')],
-    [sg.Text("Choose a folder:")],
-    [sg.Input(key="-IN2-", enable_events=True), sg.FolderBrowse()],
-    [sg.Button("Match", size=(10, 1))],
-    [sg.ProgressBar(100, visible=False, orientation='h', size=(30, 20), key='-PROGRESS_BAR-')],
-    [sg.Text("", size=(40, 1), key='-PROGRESS_LABEL-', justification='center')]
-]
+def create_main_window():
+    """
+    Creates the main window layout and returns the window object.
+    """
+    sg.theme("SandyBeach")
 
-# Create the window
-window = sg.Window('Google Photos Matcher', layout, icon='photos.ico', finalize=True)
+    layout = [
+        [sg.Text('Enter suffix used for edited photos (optional):')],
+        [sg.InputText(key='-INPUT_TEXT-'), sg.Button('Help')],
+        [sg.Text("Choose a folder:")],
+        [sg.Input(key="-IN2-", enable_events=True), sg.FolderBrowse()],
+        [sg.Button("Match", size=(10, 1))],
+        [sg.ProgressBar(100, visible=False, orientation='h', size=(30, 20), key='-PROGRESS_BAR-')],
+        [sg.Text("", size=(40, 1), key='-PROGRESS_LABEL-', justification='center')]
+    ]
 
-# Event loop
-while True:
-    event, values = window.read()
+    return sg.Window('Google Photos Matcher', layout, icon='photos.ico', finalize=True)
 
-    if event in (sg.WIN_CLOSED, "Exit"):
-        break
-    elif event == "Match":
-        # Call the main processing function with the selected folder, window object, and input text
-        main_process(values["-IN2-"], window, values['-INPUT_TEXT-'])
-    elif event == "Help":
-        # Display help information in a popup
-        sg.Popup(
-            "Media edited with the integrated editor of Google Photos will download both the original image 'Example.jpg' "
-            "and the edited version 'Example-editado.jpg'. The 'editado' suffix changes depending on the language "
-            "(e.g., 'editato' for Spanish). If you leave this box blank, the default Spanish suffix will be used.",
-            title="Information",
-            icon='photos.ico'
-        )
 
-# Close the window
-window.close()
+def main():
+    """
+    The main event loop for the application.
+    """
+    window = create_main_window()
+
+    while True:
+        event, values = window.read()
+
+        if event in (sg.WIN_CLOSED, "Exit"):
+            break
+        elif event == "Match":
+            main_process(values["-IN2-"], window, values['-INPUT_TEXT-'])
+        elif event == "Help":
+            sg.Popup(
+                "Media edited with the integrated editor of Google Photos will download both the original image 'Example.jpg' "
+                "and the edited version 'Example-editado.jpg'. The 'editado' suffix changes depending on the language "
+                "(e.g., 'editado' for Spanish). If you leave this box blank, the default Spanish suffix will be used.",
+                title="Information",
+                icon='photos.ico'
+            )
+
+    window.close()
+
+
+if __name__ == "__main__":
+    main()
